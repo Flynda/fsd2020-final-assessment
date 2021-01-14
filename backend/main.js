e9be163d915213579949d741e59b7558bdd3d591
Linda See <seewenli@gmail.com>
2021-01-15 00:06:02 +0800

    conn limit

--- backend/main.js
+++ backend/main.js
@@ -54,7 +54,7 @@ const pool = mysql.createPool({
     database: process.env.SQL_DATABASE,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
-    connectionLimit: process.env.SQL_CONNECTION_LIMIT,
+    connectionLimit: process.env.SQL_CONNECTION_LIMIT || 4,
     timezone: '+08:00',
     connectTimeout: 20000,
     waitForConnections: true,
